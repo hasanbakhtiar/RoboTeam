50a0418f6eb1affced85561b04cdf27714d55181
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-03-16 12:15:22 +0400

    function

---
+++ JavaScript/PureJS/lesson7-function/js/script.js
@@ -0,0 +1,103 @@
+// function name(params){
+//     codes
+// }
+// name();
+
+// info();
+// function info(){
+//     console.log('info running...');
+// }
+
+// const info = function(){
+//     console.log('info running...');
+// }
+// info();
+
+// (function(){
+//     console.log('function is running...');
+// })()
+
+// const info = function(){
+//     console.log('test');
+//   return  info();
+// }
+// info()
+// Not a number
+
+// function info(a=5,b=20){
+//         console.log(a+b);
+// }
+
+// info(50,100);
+
+// function info(count){
+//    return count;
+// }
+
+// console.log(info(10));
+
+// let access = true;
+
+// function infoOne(comingdata){
+//     let data = "Hello"
+//     return data+ comingdata;
+// }
+
+// function infoTwo(key,numberOne,numberTwo){
+//     let total = numberOne+numberTwo;
+
+//     if (key) {
+//         return infoOne(total);
+//     }else{
+//         return infoOne('no access')
+//     }
+
+// }
+
+// console.log(infoTwo(access,100,200));
+
+// let userName = "Jone";
+// let tableCount = "9";
+// let products = {
+//   product1: 20,
+//   product2: 40,
+// };
+
+// function calculateBill(comingPriceOne, comingPriceTwo) {
+//   let total = comingPriceOne + comingPriceTwo;
+//   return total;
+// }
+
+// function userData(un, tc, price1, price2) {
+//   if (!un || !tc) {
+//     alert("Please, fill variable");
+//   } else {
+//     return `username:${un} |table count:${tc}| total price: ${calculateBill(
+//       price1,
+//       price2
+//     )}`;
+//   }
+// }
+
+// console.log(
+//   userData(userName, tableCount, products.product1, products.product2)
+// );
+
+// function(){}
+
+// ()=>{}
+
+// const info = ()=>{
+//     console.log('hello');
+// }
+
+// info();
+
+// const App=(a)=>{
+//     this.a = a;
+// }
+// const myApp = new App(5);
+// console.log(myApp.a);
+
+
+

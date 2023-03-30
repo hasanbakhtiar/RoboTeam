bcd0e245b1405cd3ddce6938450bdb35d45ab0bf
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-03-30 12:09:55 +0400

    DOM part 1

---
+++ JavaScript/PureJS/lesson10-DOMpart1/js/script.js
@@ -0,0 +1,61 @@
+// const text = document.getElementsByTagName('h1');
+// for(let c=0;c<text.length;c++){
+//     text[c].innerHTML = 'bye';
+// }
+
+// document.getElementsByClassName('test')[0].innerHTML = "Bye"
+
+// document.getElementById('demo').innerHTML = "Bye";
+
+// document.getElementsByTagName('p')[0].innerText = '<h1 class="test">Hello</h1>';
+
+// document.querySelector('h1').innerHTML = 'bye';
+// document.querySelectorAll('.test')[1].innerHTML = 'bye';
+
+// ==================================================================================
+
+
+
+
+// import {selectors} from './selectors.js'
+
+// // console.log(text.innerHTML);
+
+// const lookCapital=()=>{
+
+//     if (selectors.text.innerHTML === 'England') {
+//         selectors.text.innerHTML = 'London';
+//         selectors.btn.innerHTML = 'country'
+        
+//     }else{
+//         selectors.text.innerHTML = 'England';
+//         selectors.btn.innerHTML = 'capital'
+
+//     }
+    
+// }
+
+// // btn.onclick = lookCapital;
+// selectors.btn.addEventListener('click',lookCapital);
+
+
+
+// document.querySelector('#g1').addEventListener('click',()=>{
+//     alert('green');
+// },false);
+
+// document.querySelector('#w1').addEventListener('click',()=>{
+//     alert('white');
+// },false);
+
+// document.querySelector('#g2').addEventListener('click',()=>{
+//     alert('green');
+// },true);
+
+// document.querySelector('#w2').addEventListener('click',()=>{
+//     alert('white');
+// },true);
+
+
+
+
---
+++ JavaScript/PureJS/lesson10-DOMpart1/js/selectors.js
@@ -0,0 +1,6 @@
+// const selectors = {
+//          text : document.querySelector('h1'),
+//          btn : document.querySelector('button')
+// }
+
+// export {selectors};

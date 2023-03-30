3f3e738b5eac257679e18dc31bd427ac0bc606fa
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-03-30 12:38:45 +0400

    module

---
+++ JavaScript/PureJS/lesson9-module-object/js/data.js
@@ -0,0 +1,29 @@
+const carListOne = [
+  {
+    brand: "BMW",
+    model: "X5",
+    year: 2022,
+  },
+  {
+    brand: "Audi",
+    model: "A4",
+    year: 2020,
+  },
+];
+
+const carListTwo = [
+    {
+      brand: "Lada",
+      model: "2106",
+      year: 2002,
+    },
+    {
+      brand: "Mercedes",
+      model: "S500",
+      year: 2021,
+    },
+  ];
+
+export {carListOne, carListTwo}
+
+
---
+++ JavaScript/PureJS/lesson9-module-object/js/script.js
@@ -0,0 +1,41 @@
+"use strict";
+// const a = 5;
+// console.log(a);
+
+//   const carOne = {
+//     brand: "BMW",
+//     model: "X5",
+//     year: 2022,
+//     calculateSpeed:function(km,hour) {
+//             return `${this.brand}  ${this.model} ${km/hour}km/h`; 
+//     }
+//   };
+
+
+// const carTwo ={
+//     brand: "Audi",
+//     model: "A4",
+//     year: 2020,
+   
+// }
+
+  
+
+// const newCalculateSpeed = carOne.calculateSpeed.bind(carTwo);
+// console.log(newCalculateSpeed(100,5));
+// console.log(carOne.calculateSpeed(200,4));
+
+// import {carListOne,carListTwo} from './data.js';
+// import * as cardata from './data.js';
+
+
+// cardata.carListOne.map(item=>{
+//     console.log(item.brand);
+// })
+
+
+// cardata.carListTwo.map(item=>{
+//     console.log(item.brand);
+// })
+// console.log(cardata);
+

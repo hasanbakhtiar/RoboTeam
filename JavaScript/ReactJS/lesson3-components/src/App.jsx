5ef468dfd37c82bf50eb8a9091a5238017f74898
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-05-11 12:07:15 +0400

    react components

---
+++ JavaScript/ReactJS/lesson3-components/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import List from './components/List'
+
+ class App extends Component {
+  render() {
+    return (
+      <>
+      <h1>hello react app</h1>
+      <List />
+      </>
+    )
+  }
+}
+
+export default App
---
+++ JavaScript/ReactJS/lesson3-components/src/components/List.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class List extends Component {
+  render() {
+    return (
+      <ol>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+      </ol>
+    )
+  }
+}
+
+export default List

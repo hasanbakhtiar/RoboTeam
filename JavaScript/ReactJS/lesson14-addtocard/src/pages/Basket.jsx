b7a3d2cb9c2d3dae1e09a530a26361ada75cd40a
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-06-26 12:31:43 +0400

    add to cart

--- JavaScript/ReactJS/lesson14-addtocard/src/pages/Basket.jsx
+++ JavaScript/ReactJS/lesson14-addtocard/src/pages/Basket.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import { Button } from 'react-bootstrap';
 import { useCart } from 'react-use-cart'
-
+import { ToastContainer, toast } from 'react-toastify';
 const Basket = () => {
     const { isEmpty,items,updateItemQuantity,removeItem,emptyCart,cartTotal } = useCart();
+    const notify = () => toast.error('🦄 Product deleted', {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+  });;
     return (isEmpty ? <div className="d-flex justify-content-center">
         <img src='https://cdnl.iconscout.com/lottie/premium/thumb/shopping-bag-6866084-5624247.gif'alt='err'/>
     </div> :
@@ -32,10 +42,22 @@ const Basket = () => {
    <span className='mx-2'>{item.quantity}</span>
    <Button variant='success' onClick={()=>{updateItemQuantity(item.id, item.quantity + 1)}}>+1</Button>
  </td>
- <td><Button variant='danger' onClick={()=>{removeItem(item.id)}}>X</Button></td>
+ <td><Button variant='danger' onClick={()=>{notify(removeItem(item.id))}}>X</Button></td>
+ 
 </tr>
     ))}
-   
+   <ToastContainer
+                        position="bottom-right"
+                        autoClose={5000}
+                        hideProgressBar={false}
+                        newestOnTop={false}
+                        closeOnClick
+                        rtl={false}
+                        pauseOnFocusLoss
+                        draggable
+                        pauseOnHover
+                        theme="dark"
+                    />
 
   </tbody>
 </table>
